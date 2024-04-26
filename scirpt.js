a454b0bee3e3d3a87aeb89982af94777286f37eb
pipocapixel pedro bispo <pipocapixel.psl@gmail.com>
2024-04-26 14:38:15 +0000

    2 commit do java (erro no meu java kk)

--- scirpt.js
+++ scirpt.js
@@ -1,10 +1,10 @@
-let botoes = document.querySelectorAll(".botoes button")
+let botoes = document.querySelectorAll(".botoes button");
 botoes.forEach(botao => {
-    botao.addEventListener("click", guardarNota)
-})
+    botao.addEventListener("click", guardarNota);
+});
 
-function guardarNota(evento){
+function guardarNota(evento) {
     let nota = evento.target.innerText
 
-    localStorage.setItem("nota", nota)
+    localStorage.setItem("nota", nota);
 }
